d74933b35b81ed80cf8f86441dafb2c1f9b7ecbc
Aske Ejlertsen <roede-aske@hotmail.com>
2022-02-25 10:50:29 +0100

    commit

--- lommeregner.py
+++ lommeregner.py
@@ -1,2 +1,24 @@
 #lommeregner opgave
 
+#bruger skriver et tal (med eller uden operator foran)
+
+
+tal1 = input("Skriv et tal: \n")
+
+#bruger vælger en operator
+
+operator = input("Vælg en operator: \n")
+
+while((tal1 or operator) != ('=' or 'c')):
+    
+    
+
+#bruger skriver et tal mere
+
+
+#bruger vælger at vise resultat(sum-knappen) eller mulighed for at skrive endnu en operator (uendeligt antal)
+
+#bruger har mulighed for at skrive endnu et tal (uendeligt antal)
+
+#bruger rydder input
+
