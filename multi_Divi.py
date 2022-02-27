dd2f30b2f3bd96c7ee22915c2367220d19e42bf2
coookieman <100272671+coookieman@users.noreply.github.com>
2022-02-27 19:53:50 +0100

    commit

---
+++ multi_Divi.py
@@ -0,0 +1,10 @@
+# Class til Multiplikation og Division
+class multiDivi:
+
+    # Multiplikation funktion
+    def multiplikation(x, y):
+        return x * y
+
+    # Division funktion
+    def division(x, y):
+        return x / y
