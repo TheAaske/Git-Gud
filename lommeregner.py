fa313c1e83aa389c15ea152b36db5b6d0ec2abe4
Aske Ejlertsen <roede-aske@hotmail.com>
2022-02-27 20:35:05 +0100

    Update lommeregner.py

--- lommeregner.py
+++ lommeregner.py
@@ -1,15 +1,6 @@
-#lommeregner opgave
-import keyboard
-from asyncio.windows_events import NULL
- 
+# Lommeregner opgave
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Importerer classen: plusMinus, fra filen plus_minus.py
-from plus_minus import plusMinus
-# Importere classen: multiDivi, fra filen multi_Divi
-from multi_Divi import multiDivi
-=======
+
 # Plus funktion
 def plus(x, y):
     return x + y
@@ -17,13 +8,14 @@ def plus(x, y):
 # Minus funktion
 def minus(x, y):
     return x - y
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of f69bb66 (commit)
-=======
->>>>>>> parent of f69bb66 (commit)
-=======
->>>>>>> parent of f69bb66 (commit)
+
+# Multiplikation funktion
+def multiplikation(x, y):
+    return x * y
+
+# Division funktion
+def division(x, y):
+    return x / y
 
 # En lille menu til lommeregner-funktioner
 print("Vælg en operator")
@@ -43,55 +35,21 @@ while True:
         tal2 = float(input("Indtast andet nummer: \n"))
     
         if(menuValg == '1'):
-            print(tal1, "+", tal2, "=", plusMinus.plus(tal1, tal2))
+            print(tal1, "+", tal2, "=", plus(tal1, tal2))
         elif(menuValg == '2'):
-            print(tal1, "+", tal2, "=", plusMinus.minus(tal1, tal2))
+            print(tal1, "+", tal2, "=", minus(tal1, tal2))
         elif(menuValg == '3'):
-            print(tal1, "+", tal2, "=", multiDivi.multiplikation(tal1, tal2))
+            print(tal1, "+", tal2, "=", multiplikation(tal1, tal2))
         elif(menuValg == '4'):
             rest = int(tal1) % int(tal2)
-            print(tal1, "+", tal2, "=", multiDivi.division(tal1, tal2), "\nmed en rest på: ", rest)
+            print(tal1, "+", tal2, "=", division(tal1, tal2), "\nmed en rest på: ", rest)
 
         # Se efter om brugeren vil regne videre
         regneMere = input("Vil du lave endnu en udregning? (ja/nej): \n")
         # Stop hvis man skriver nej
         if regneMere == "nej":
             break
-=======
-tal1 = NULL
-tal2 = NULL
-operator1 = NULL
-resultat = 0
-forfra = NULL
-stoppe = 0
-
-print("Tryk enter for at gå videre \n")
-print("Tryk 'C' og enter for at starte forfra")
-print("Tryk 'Q' og enter for at slutte")
-
-
-while(keyboard.on_press_key(key='q' ) != 'q'):
-    print("Start")
-    tal1 = input("Skriv et tal: \n")
-    operator1 = input("Skriv en operator: \n")
-    tal2 = input("Skriv et tal mere: \n")
     
->>>>>>> parent of 0bed2cb (commmit on master)
-    
-    if((operator1 == 'Q') or (tal1 == 'Q') or (tal2 == 'Q')):
-        break
-    elif(operator1 == '+'):
-        resultat = int(tal1) + int(tal2)
-        print('Resultatet er : ' + str(resultat) )
-    elif(operator1 == '-'):
-        resultat = int(tal1) - int(tal2)
-        print('Resultatet er : ' + str(resultat))
-    elif(operator1 == '*'):
-        resultat = int(tal1) * int(tal2)
-        print('Resultatet er : ' + str(resultat))
-    elif(operator1 == '/'):
-        resultat = int(tal1) / int(tal2)
-        rest = int(tal1) % int(tal2)
-        print('Resultatet er : ' + str(resultat))
-        print('Med en rest på: ' + str(rest)) 
-    
+    # Hvis inputtet ikke er en af menupunkterne skriver den fejl
+    else:
+        print("Input er ikke et menupunkt!")
